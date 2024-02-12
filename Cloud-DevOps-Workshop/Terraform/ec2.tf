10670c6f657b1b96042d588b51e3f3938bcf9c7f
ShivkumarDiwate <shivkumardiwate@gmail.com>
2024-02-12 21:59:52 +0530

    second commit

--- Cloud-DevOps-Workshop/Terraform/ec2.tf
+++ Cloud-DevOps-Workshop/Terraform/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "instance1" {
-  ami                     = "ami-087c4d241dd19276d"
-  instance_type           = "t2.micro"
-  iam_instance_profile = ""
+  ami               = "ami-0449c34f967dbf18a"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
   tags = {
-    name = "Terraform instance 1"
+    name = "Terraform-instance-1"
   }
 }
--- Cloud-DevOps-Workshop/Terraform/s3.tf
+++ Cloud-DevOps-Workshop/Terraform/s3.tf
@@ -1,7 +1,3 @@
-# provider "aws" {
-#   region     = "ap-south-1"
-# }
-
 resource "aws_s3_bucket" "shivkumarbucket" {
   bucket = "shivkumartestbucket"
 
