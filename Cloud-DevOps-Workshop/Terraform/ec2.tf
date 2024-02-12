551a769788e30c5d57655f34c62e01ff930fae14
ShivkumarDiwate <shivkumardiwate@gmail.com>
2024-02-12 21:29:52 +0530

    second commit

--- Cloud-DevOps-Workshop/Terraform/ec2.tf
+++ Cloud-DevOps-Workshop/Terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance1" {
-  ami                     = "ami-05552d2dcf89c9b24"
+  ami                     = "ami-087c4d241dd19276d"
   instance_type           = "t2.micro"
   iam_instance_profile = ""
   tags = {
