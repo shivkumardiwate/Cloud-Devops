6a78e3b49996503923b09962e462c07f7b14a895
ShivkumarDiwate <shivkumardiwate@gmail.com>
2024-02-14 21:02:19 +0530

    test3

--- Cloud-DevOps-Workshop/Terraform/ec2.tf
+++ Cloud-DevOps-Workshop/Terraform/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance1" {
   ami               = "ami-0449c34f967dbf18a"
   instance_type     = "t2.micro"
   availability_zone = "ap-south-1a"
+  key_name          = "instance1"
   tags = {
     name = "Terraform-instance-1"
   }
